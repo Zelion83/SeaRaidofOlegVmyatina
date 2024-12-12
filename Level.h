998cbc3364d3065294a34815f6242eb9fc7b6871
Zelion83 <zegpb21@gmail.com>
2024-12-12 17:19:32 +0500

    trying to understand the git

---
+++ Level.h
@@ -0,0 +1,41 @@
+#pragma once
+#include<vector>
+#include "Button.h"
+class Level {
+protected:
+    int currentrow = -1;
+    int currentbutton = -1;
+    std::vector<std::vector<Button>> buttons;
+public:
+    void addButtonRow(std::vector<Button> row) {
+		buttons.push_back(row);
+	}
+	
+    
+    virtual void manageButton() {
+        currentrow = -1;
+        currentbutton = -1;
+        for (int i = 0; i < buttons.size(); i++) {
+            for (int j = 0; j < buttons[i].size(); j++) {
+                if (buttons[i][j].is_active) {
+                    currentrow = i;
+                    currentbutton = j;
+                    break;
+                }
+            }
+            if (currentrow != -1) break;
+        }
+        
+        if (currentrow == -1) return; 
+        
+        int newrow = currentrow;
+        int newbutton = currentbutton;
+    }
+    virtual void update() {
+        for (auto& i : buttons) {
+            for (auto& j : i) {
+                j.update(ren);
+            }
+        }
+    }
+};
---
+++ quick_stats.h
@@ -0,0 +1,38 @@
+#pragma once
+#include"Button.h"
+#include"Ship&Sailor.h"
+class Quick_stats{
+	Text crew, morale, gold, power;
+public:
+	Quick_stats(Ship& ship,SDL_Renderer* ren):
+		crew(48, L"Размер экипажа: ", ren, { 30,900,500,100 }), morale(48, L"Мораль экипажа:", ren, {600,900,500,100}),
+		gold(48, L"Золото: ", ren, {30,950,500,100 }), power(48, L"Сила корабля: ", ren, { 600,950,500,100 })
+	{
+		std::wstring ws = L"Размер экипажа: " + std::to_wstring(ship.get_cursize());
+		crew.rename(ws.c_str(),ren);
+		ws = L"Мораль экипажа: " + std::to_wstring(ship.morale);
+		morale.rename(ws.c_str(), ren);
+		ws = L"Золото: " + std::to_wstring(ship.get_gold());
+		gold.rename(ws.c_str(), ren);
+		ws = L"Сила корабля: " + std::to_wstring(ship.power);
+		//power = Text(48, L"Сила корабля: ", ren, { 600,950,500,100 }); вот так нельзя инициализировать
+		power.rename(ws.c_str(), ren);
+
+	}
+	void show(SDL_Renderer* ren){
+		crew.render(ren);
+		morale.render(ren);
+		gold.render(ren);
+		power.render(ren);
+	}
+	void update(Ship& ship) {
+		std::wstring ws = L"Размер экипажа: " + std::to_wstring(ship.get_cursize());
+		crew.rename(ws.c_str(), ren);
+		ws = L"Мораль экипажа: " + std::to_wstring(ship.morale);
+		morale.rename(ws.c_str(), ren);
+		ws = L"Золото: " + std::to_wstring(ship.get_gold());
+		gold.rename(ws.c_str(), ren);
+		ws = L"Сила корабля: " + std::to_wstring(ship.power);
+		power.rename(ws.c_str(), ren);
+	}
+};
